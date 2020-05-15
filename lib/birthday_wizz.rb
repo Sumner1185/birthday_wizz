d08cf5d3650afc184cd63a4de022b9ea7e5ce4f2
Sumner1185 <alastair.sumner@yahoo.com>
2020-05-15 10:59:20 +0100

    Test to add a birthday

--- lib/birthday_wizz.rb
+++ lib/birthday_wizz.rb
@@ -1,7 +1,11 @@
 class BirthdayWizz
 
-  def input_birthdays
-  
+  def initialize
+    @list = []
+  end
+
+  def input_birthdays(name, date)
+    @list.push({name: name, date: date})
   end
 
 end
--- spec/birthday_wizz_spec.rb
+++ spec/birthday_wizz_spec.rb
@@ -7,4 +7,8 @@ describe BirthdayWizz do
     expect(subject).to respond_to :input_birthdays
   end
 
+  it 'should add a birthday' do
+    expect(subject.input_birthdays("Alastair Sumner", "13th November 1985")).to eq [{name: "Alastair Sumner", date: "13th November 1985"}]
+  end
+
 end
