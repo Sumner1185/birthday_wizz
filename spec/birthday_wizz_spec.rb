cfdb9b65394c30bd982fcd4b1a08cd65c2c705a8
Sumner1185 <alastair.sumner@yahoo.com>
2020-05-19 13:56:09 +0100

    Refactor respond_to test

--- spec/birthday_wizz_spec.rb
+++ spec/birthday_wizz_spec.rb
@@ -3,9 +3,7 @@ require_relative '../lib/birthday_wizz'
 describe BirthdayWizz do
   let(:subject) {BirthdayWizz.new}
   
-  it 'should respond to input_birthdays' do
-    expect(subject).to respond_to :input_birthday
-  end
+  it { is_expected.to respond_to :input_birthday }
 
   it 'should add a birthday' do
     expect(subject.input_birthday("Alastair Sumner", "13th November 1985")).to eq [{name: "Alastair Sumner", date: "13th November 1985"}]
@@ -17,11 +15,12 @@ describe BirthdayWizz do
   end
 
   it 'should show all birthdays' do
-    list = BirthdayWizz.new
-    list.input_birthday("Alastair Sumner", "13th November 1985")
-    list.input_birthday("Fay Pantry", "25th June 1991")
-
-    expect { list.show_birthday }.to output("Alastair Sumner: 13th November 1985\nFay Pantry: 25th June 1991\n").to_stdout
+    subject.input_birthday("Alastair Sumner", "13th November 1985")
+    subject.input_birthday("Fay Pantry", "25th June 1991")
+      expect { subject.show_birthday }.to output("Alastair Sumner: 13th November 1985\nFay Pantry: 25th June 1991\n").to_stdout
   end
 
+  it 'should check to see who\'s birthday is today' do
+
+  end
 end
