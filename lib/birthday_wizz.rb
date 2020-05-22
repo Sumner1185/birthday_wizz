961a5fd9bd5848b9ab192ed34e13ae19cd633fdb
Sumner1185 <alastair.sumner@yahoo.com>
2020-05-22 12:33:21 +0100

    Finally got timecop working and tests green

--- lib/birthday_wizz.rb
+++ lib/birthday_wizz.rb
@@ -1,10 +1,13 @@
+require 'date'
+
 class BirthdayWizz
+  FORMAT_DATE = '%m-%d'
 
   def initialize
     @list = []
   end
 
-  def input_birthday(name, date)
+  def add(name, date)
     @list.push({name: name, date: date})
   end
 
@@ -12,6 +15,28 @@ class BirthdayWizz
     @list.each do |person|
       puts "#{person[:name]}: #{person[:date]}"
     end
+  end
+
+  def check_today
+    @list.each do |bday|
+      puts "It's #{bday[:name]}'s birthday today! They are #{age(bday)} years old" if today?(bday)
+    end
+  end
+
+  def age(bday)
+    Time.now.year - Date.parse(bday[:date]).year
+  end
+
+  def today?(bday)
+    birthday_date(bday) == today
+  end
+
+  def birthday_date(bday)
+    Date.parse(bday[:date]).strftime(FORMAT_DATE)
+  end
+
+  def today
+    Time.now.strftime(FORMAT_DATE)
   end
 
 end
--- spec/birthday_wizz_spec.rb
+++ spec/birthday_wizz_spec.rb
@@ -1,26 +1,32 @@
 require_relative '../lib/birthday_wizz'
+require 'timecop'
 
 describe BirthdayWizz do
-  let(:subject) {BirthdayWizz.new}
+  let(:list) {BirthdayWizz.new}
   
-  it { is_expected.to respond_to :input_birthday }
+  it { is_expected.to respond_to :add }
 
   it 'should add a birthday' do
-    expect(subject.input_birthday("Alastair Sumner", "13th November 1985")).to eq [{name: "Alastair Sumner", date: "13th November 1985"}]
+    expect(list.add("Alastair Sumner", "13 November 1985")).to eq [{name: "Alastair Sumner", date: "13 November 1985"}]
   end
 
   it 'should show a birthday' do
-    subject.input_birthday("Alastair Sumner", "13th November 1985")
-    expect { subject.show_birthday }.to output("Alastair Sumner: 13th November 1985\n").to_stdout
+    list.add("Alastair Sumner", "13 November 1985")
+    expect { list.show_birthday }.to output("Alastair Sumner: 13 November 1985\n").to_stdout
   end
 
   it 'should show all birthdays' do
-    subject.input_birthday("Alastair Sumner", "13th November 1985")
-    subject.input_birthday("Fay Pantry", "25th June 1991")
-      expect { subject.show_birthday }.to output("Alastair Sumner: 13th November 1985\nFay Pantry: 25th June 1991\n").to_stdout
+    list.add("Alastair Sumner", "13 November 1985")
+    list.add("Fay Pantry", "25 June 1991")
+      expect { list.show_birthday }.to output("Alastair Sumner: 13 November 1985\nFay Pantry: 25 June 1991\n").to_stdout
   end
 
-  it 'should check to see who\'s birthday is today' do
-
+  it 'should check the list to see who\'s birthday is today' do
+    Timecop.freeze(Time.parse('13 November 2019')) do
+      
+      list.add("Alastair Sumner", "13 November 1985")
+      list.add("Diane Salisbury", "23 May 1961")
+       expect { list.check_today }.to output("It's Alastair Sumner's birthday today! They are 34 years old\n").to_stdout
+    end
   end
 end
